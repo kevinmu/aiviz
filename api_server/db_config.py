3b33fa74c8c1bd357caf4ed81700ed2b334adf85
Kevin Mu <kevin.mu@rubrik.com>
2023-10-15 20:28:33 -0700

    fix db_config

--- api_server/db_config.py
+++ api_server/db_config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql://root:@localhost/AIVIZ"
+DATABASE_URL = "mysql://sqlalchemy:sqlalchemy_pw@localhost/AIVIZ"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
